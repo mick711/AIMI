2f84569313fc6cfc5017c8925a037c568c8b7414
MTR93600 <tellier@pm.me>
2021-05-17 15:20:16 +0200

    initial commit aimi-tsunami

---
+++ app/build/generated/source/buildConfig/full/release/info/nightscout/androidaps/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.nightscout.androidaps;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "info.nightscout.androidaps";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "full";
+  public static final int VERSION_CODE = 1500;
+  public static final String VERSION_NAME = "2.8.2-UAM-tsunami-0.6.1";
+  // Fields from default config.
+  public static final String BUILDVERSION = "77f38fa6-2021.05.17-11:13";
+  public static final String COMMITTED = "false";
+  public static final String HEAD = "77f38fa6";
+  public static final String REMOTE = "https://gitlab.com/piecycle/aaps_uam_tsunami.git";
+  public static final String VERSION = "2.8.2-UAM-tsunami-0.6.1";
+}
